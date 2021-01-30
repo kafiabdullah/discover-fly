25427b014e3d46cd99e346624ac646e41898270e
kafi <abdullahkafi996@gmail.com>
2021-01-30 21:49:44 +0600

    add calculation method for first class

--- js/main.js
+++ js/main.js
@@ -19,3 +19,37 @@ const vatAmount = document.getElementById('vat');
 
 // total price
 const totalAmount = document.getElementById('total');
+
+// Extract the value from the document
+let firstClassValue = isEmptyAndSetDefault(firstClassInput);
+let economyClassValue = isEmptyAndSetDefault(economyClassInput);
+
+
+// Ticket Price
+const firstClassTicketPrice = 150;
+const economyClassTicketPrice = 100;
+
+// Calculation
+let totalFirstClassPrice = 0;
+let totalEconomyClassPrice = 0;
+let vat = 0;
+
+
+// Increment the firstClassValue
+firstClassIncrementBtn.addEventListener("click", function () {
+    firstClassValue++;
+
+    // Total FirstClass Ticket Price
+    totalFirstClassPrice = updatePriceCalculation(
+        firstClassInput,
+        firstClassValue,
+        firstClassTicketPrice
+    );
+
+    let totalTicketPrice = totalFirstClassPrice + totalEconomyClassPrice;
+
+    // Calculate the vat
+    vat = totalTicketPrice * 0.1;
+
+    updatePriceSummary(totalTicketPrice, vat);
+});
