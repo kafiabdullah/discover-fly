6fde5eb4978b43553ae8b7e4223c69f0bb81d9be
kafi <abdullahkafi996@gmail.com>
2021-01-30 21:42:38 +0600

    add id to get update jsvascript

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// first class btn
+const firstClassIncrementBtn = document.getElementById('first-class-increment');
+const firstClassDecrementBtn = document.getElementById('first-class-decrement');
+
+
+// economy class btn
+const economyClassIncrementBtn = document.getElementById('economy-class-increment');
+const economyClassDecrementBtn = document.getElementById('economy-class-decrement');
+
+// input 
+const firstClassInput = document.getElementById('first-class-input');
+const economyClassInput = document.getElementById('economy-class-input');
+
+// subtotal
+const subtotalAmount = document.getElementById('price-subtotal');
+
+// vat
+const vatAmount = document.getElementById('vat');
+
+// total price
+const totalAmount = document.getElementById('total');
